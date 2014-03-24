a406cb136ec9f503bcf57c2dd29f2a05db365954
BruceZCQ <brucezcq@gmail.com>
2014-03-24 18:08:59 +0800

    begin work opodb

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpODB
+//
+//  Created by BruceZCQ on 14-3-24.
+//  Copyright (c) 2014年 OpeningO,Inc (http://openingo.github.io/). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
